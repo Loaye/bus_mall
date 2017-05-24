b7a50a4fb177241af2e98cdd18d3c64d2e72fe2d
Loaye <jr.carstens00@gmail.com>
2017-05-24 13:32:30 -0700

    added user stories

--- app.js
+++ app.js
@@ -10,17 +10,6 @@ function Product(name, url){
   this.timesselected = 0;
 }
 
-
-
-Product.prototype.pickedMe = function(){
-  counter++;
-  this.timesclicked++;
-};
-
-Product.prototype.beenChosen = function(){
-  this.timesselected++;
-};
-
 //Create array of product objects//
 var bag = new Product('bag', 'image/bag.jpg');
 var banana = new Product('banana', 'image/banana.jpg');
@@ -30,14 +19,14 @@ var breakfast = new Product('breakfast', 'image/breakfast.jpg');
 var bubblegum = new Product('bubblegum', 'image/bubblegum.jpg');
 var chair = new Product('chair', 'image/chair.jpg');
 var cthulhu = new Product('cthulhu', 'image/cthulhu.jpg');
-var dog_duck = new Product('dog_duck', 'image/dog_duck.jpg');
+var dogDuck = new Product('dogDuck', 'image/dog-duck.jpg');
 var dragon = new Product('dragon', 'image/dragon.jpg');
 var pen = new Product('pen', 'image/pen.jpg');
-var pet_sweep = new Product('pet_sweep', 'image/pet_sweep.jpg');
+var petSweep = new Product('petSweep', 'image/pet-sweep.jpg');
 var scissors = new Product('scissors', 'image/scissors.jpg');
 var shark = new Product('shark', 'image/shark.jpg');
 var sweep = new Product('sweep', 'image/sweep.png');
 var tauntaun = new Product('tauntaun', 'image/tauntaun.jpg');
 var unicorn = new Product('unicorn', 'image/unicorn.jpg');
 var usb = new Product('usb', 'image/usb.gif');
-var water_can = new Product('water_can', 'image/water_can.jpg');
+var waterCan = new Product('waterCan', 'image/water-can.jpg');
--- user_stories.md
+++ user_stories.md
@@ -1,16 +1,10 @@
 Developer:
 
-As a developer,
-As a developer,
-As a developer,
-As a developer,
+As a developer, I want to write code so that not only I can read it but others who work with me can.
+As a developer, I want to be simple and minimalistic.
 
-As the Marketing Team,
-As the Marketing Team,
-As the Marketing Team,
-As the Marketing Team,
+As the Marketing Team, we want to develop an amazing UI and UX.
+As the Marketing Team, we want to find out how many people like set items.
 
-As a user,
-As a user,
-As a user,
-As a user, 
+As a user, I want a good user experience with good visuals.
+As a user, I want to find products that fits to my interests.
