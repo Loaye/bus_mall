088317fbcb6d4f70029d7a889787604d79bddf7d
Loaye <jr.carstens00@gmail.com>
2017-05-25 22:51:09 -0700

    final product

--- library/app.js
+++ library/app.js
@@ -22,26 +22,31 @@ Product.prototype.percentage = function() {
   return (this.clickCounter / this.displayCounter) * 100.0;
 };
 
-//Instantiatiing new products
-var bag = new Product('bag', 'image/bag.jpg');
-var banana = new Product('banana', 'image/banana.jpg');
-var bathroom = new Product('bathroom', 'image/bathroom.jpg');
-var boots = new Product('boots', 'image/boots.jpg');
-var breakfast = new Product('breakfast', 'image/breakfast.jpg');
-var bubblegum = new Product('bubblegum', 'image/bubblegum.jpg');
-var chair = new Product('chair', 'image/chair.jpg');
-var cthulhu = new Product('cthulhu', 'image/cthulhu.jpg');
-var dogDuck = new Product('dogDuck', 'image/dog-duck.jpg');
-var dragon = new Product('dragon', 'image/dragon.jpg');
-var pen = new Product('pen', 'image/pen.jpg');
-var petSweep = new Product('petSweep', 'image/pet-sweep.jpg');
-var scissors = new Product('scissors', 'image/scissors.jpg');
-var shark = new Product('shark', 'image/shark.jpg');
-var sweep = new Product('sweep', 'image/sweep.png');
-var tauntaun = new Product('tauntaun', 'image/tauntaun.jpg');
-var unicorn = new Product('unicorn', 'image/unicorn.jpg');
-var usb = new Product('usb', 'image/usb.gif');
-var waterCan = new Product('waterCan', 'image/water-can.jpg');
+if (localStorage.productsArray) {
+  productsArray = JSON.parse(localStorage.productsArray);
+  clicks = JSON.parse(localStorage.clicks);
+} else {
+  //Instantiatiing new products
+  var bag = new Product('bag', 'image/bag.jpg');
+  var banana = new Product('banana', 'image/banana.jpg');
+  var bathroom = new Product('bathroom', 'image/bathroom.jpg');
+  var boots = new Product('boots', 'image/boots.jpg');
+  var breakfast = new Product('breakfast', 'image/breakfast.jpg');
+  var bubblegum = new Product('bubblegum', 'image/bubblegum.jpg');
+  var chair = new Product('chair', 'image/chair.jpg');
+  var cthulhu = new Product('cthulhu', 'image/cthulhu.jpg');
+  var dogDuck = new Product('dogDuck', 'image/dog-duck.jpg');
+  var dragon = new Product('dragon', 'image/dragon.jpg');
+  var pen = new Product('pen', 'image/pen.jpg');
+  var petSweep = new Product('petSweep', 'image/pet-sweep.jpg');
+  var scissors = new Product('scissors', 'image/scissors.jpg');
+  var shark = new Product('shark', 'image/shark.jpg');
+  var sweep = new Product('sweep', 'image/sweep.png');
+  var tauntaun = new Product('tauntaun', 'image/tauntaun.jpg');
+  var unicorn = new Product('unicorn', 'image/unicorn.jpg');
+  var usb = new Product('usb', 'image/usb.gif');
+  var waterCan = new Product('waterCan', 'image/water-can.jpg');
+}
 
 //Random Number Generator to be assigned to a picture
 function randomNum() {
@@ -89,6 +94,15 @@ function newClick(e){
   }
   render();
   stop();
+}
+
+function save() {
+  JSON.stringify(productsArray);
+  JSON.stringify(clicks);
+  localStorage.productsArray = productsArray;
+  localStorage.clicks = clicks;
+  console.log('product array:', productsArray);
+  console.log('localStorage list:', localStorage.productsArray);
 }
 
 function stop(){
@@ -148,6 +162,7 @@ function results() {
   });
 }
 
+save();
 render();
 
 productLeft.addEventListener('click', newClick);
